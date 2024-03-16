b1f9992545f1430bdbc316f307ff8f3b21d4bc00
MohamedEL-gantery <elgantery50@gmail.com>
2024-03-16 17:41:31 +0200

    edit in multer file

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,22 +1,19 @@
 const multer = require('multer');
 const AppError = require('../utils/appError');
 
-// Set up multer storage
 const storage = multer.memoryStorage();
 
-// Define a filter function to check file types
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image/')) {
-    cb(null, true); // Accept the file
+    cb(null, true);
   } else {
     cb(
       new AppError('File type not supported. Only images are allowed.', 401),
       false
-    ); // Reject the file
+    );
   }
 };
 
-// Configure multer with storage and filter
 const upload = multer({
   storage: storage,
   fileFilter: multerFilter,
